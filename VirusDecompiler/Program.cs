8b9b61a9a090ad8ecfec2b611c033357ac64bbd9
george <george.bungarzescu@softeh.ro>
2016-03-14 15:00:05 +0200

    viral code generated

--- VirusDecompiler/Program.cs
+++ VirusDecompiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 
 namespace VirusDecompiler
@@ -13,9 +14,9 @@ namespace VirusDecompiler
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            ViralCodes.ExtractAllToFiles();
+
+           
         }
     }
 }
